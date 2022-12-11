0ef0214c3883b8f49dc8ff9525b522f32369c857
stellan-s <stellan.sarnblom@pridac.se>
2022-12-11 13:38:21 +0100

    Syntax sugar fix

--- firework.js
+++ firework.js
@@ -1,51 +1,53 @@
-function Firework() {
-  this.hue = random(70, 340);
-  this.firework = new Particle(random(width), height, this.hue, true);
-  this.exploded = false;
-  this.particles = [];
+class Firework {
+  constructor() {
+    this.hue = random(70, 340);
+    this.firework = new Particle(random(width), height, this.hue, true);
+    this.exploded = false;
+    this.particles = [];
 
-  this.update = function () {
-    if (!this.exploded) {
-      this.firework.applyForce(gravity);
-      this.firework.update();
-      if (this.firework.vel.y >= 0) {
-        this.exploded = true;
-        this.explode();
+    this.update = function () {
+      if (!this.exploded) {
+        this.firework.applyForce(gravity);
+        this.firework.update();
+        if (this.firework.vel.y >= 0) {
+          this.exploded = true;
+          this.explode();
+        }
       }
-    }
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      this.particles[i].applyForce(gravity);
-      this.particles[i].update();
-      if (this.particles[i].done()) {
-        this.particles.splice(i, 1);
+      for (let i = this.particles.length - 1; i >= 0; i--) {
+        this.particles[i].applyForce(gravity);
+        this.particles[i].update();
+        if (this.particles[i].done()) {
+          this.particles.splice(i, 1);
+        }
       }
-    }
-  };
+    };
 
-  this.explode = function () {
-    let color = random(10);
-    for (let i = 0; i < random(360, 700); i++) {
-      let p = new Particle(
-        this.firework.pos.x,
-        this.firework.pos.y,
-        color >= 7 ? random(80, 360) : this.hue,
-        false
-      );
-      this.particles.push(p);
-    }
-  };
+    this.explode = function () {
+      let color = random(10);
+      for (let i = 0; i < random(360, 700); i++) {
+        let p = new Particle(
+          this.firework.pos.x,
+          this.firework.pos.y,
+          color >= 7 ? random(80, 360) : this.hue,
+          false
+        );
+        this.particles.push(p);
+      }
+    };
 
-  this.done = function () {
-    if (this.exploded && this.particles.length === 0) return true;
-    else return false;
-  };
+    this.done = function () {
+      if (this.exploded && this.particles.length === 0) return true;
+      else return false;
+    };
 
-  this.show = function () {
-    if (!this.exploded) {
-      this.firework.show();
-    }
-    this.particles.forEach((f) => {
-      f.show();
-    });
-  };
+    this.show = function () {
+      if (!this.exploded) {
+        this.firework.show();
+      }
+      this.particles.forEach((f) => {
+        f.show();
+      });
+    };
+  }
 }
